ed9486fc5b025a6875a645bd84aca03ca3c042f4
Corey Snyder <corey.snyder@joinroot.com>
2022-01-07 16:40:08 -0500

    Adds food_finder app files

---
+++ food_finder/spec/support/number_helper_spec.rb
@@ -0,0 +1,43 @@
+# encoding: UTF-8
+
+describe 'NumberHelper' do
+
+  include NumberHelper
+
+  describe '#number_to_currency' do
+
+    context 'using default values' do
+
+      it "correctly formats an integer" do
+        expect(number_to_currency(2000)).to eq('$2,000.00')
+      end
+
+      it "correctly formats a float"
+
+      it "correctly formats a string"
+
+      it "uses delimiters for very large numbers"
+
+      it "does not have delimiters for small numbers"
+
+    end
+
+    context 'using custom options' do
+
+      it 'allows changing the :unit'
+
+      it 'allows changing the :precision'
+
+      it 'omits the separator if :precision is 0'
+
+      it 'allows changing the :delimiter'
+
+      it 'allows changing the :separator'
+
+      it 'correctly formats using multiple options'
+
+    end
+
+  end
+
+end
